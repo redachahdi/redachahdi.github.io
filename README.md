faf6c923214036f944f91324d88dcfc0dfe643a8
redachahdi <reda.elkhattabi01@gmail.com>
2023-12-13 15:55:22 +0100

    clean the code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # redachahdi.github.io
-landing page
+
+# Clothing Store Project
+
+This project is a simple web page for a clothing store, featuring a responsive design and interactive elements. It includes HTML for the structure, CSS for styling, and JavaScript for dynamic behavior.
+
+## Files
+
+- **index.html**: Main HTML file containing the structure of the web page.
+- **cours.css**: CSS file for styling the web page.
+- **reda.js**: JavaScript file for adding interactivity to the web page.
+
+## Description
+
+- **HTML (index.html):**
+
+  - Defines the structure of the web page, including header, main content, and footer.
+  - Features product sections, a navigation menu, and a payment section.
+
+- **CSS (cours.css):**
+
+  - Resets default browser styles.
+  - Defines styles for the body, header, navigation, main content, product sections, buttons, and footer.
+  - Includes specific styles for the payment section (to be added).
+
+- **JavaScript (reda.js):**
+  - Listens for the DOMContentLoaded event before executing.
+  - Adds interactivity to the page:
+    - Changes the header background color on click.
+    - Alerts a message when a product's "Add to Cart" button is clicked.
+    - Dynamically updates the footer text with the current year.
+    - Enlarges product images on mouseover and reverts on mouseout.
+
+## Usage
+
+- Open `index.html` in a web browser to view the clothing store web page.
+- Interact with the page by clicking on the header, adding products to the cart, and observing dynamic changes.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+© 2023 Clothing Store
--- reda.js
+++ reda.js
@@ -1,37 +1,28 @@
 // reda.js
 
+// Wait for the DOM content to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function () {
     // Change the background color of the header on click
     const header = document.querySelector("header");
     header.addEventListener("click", function () {
-      header.style.backgroundColor = "#555";
+        header.style.backgroundColor = "#555"; // Change header background color to gray (#555) on click
     });
-  
+
     // Add an item to the cart when a product's "Add to Cart" button is clicked
     const addToCartButtons = document.querySelectorAll(".product button");
     addToCartButtons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        alert("Product added to cart!");
-        // You can add more complex logic here, such as updating a shopping cart object.
-      });
+        button.addEventListener("click", function () {
+            alert("Product added to cart!"); // Show an alert when the "Add to Cart" button is clicked
+            // You can add more complex logic here, such as updating a shopping cart object.
+        });
     });
-  
-    // Change the footer text dynamically
+
+    // Change the footer text dynamically to include the current year
     const footer = document.querySelector("footer p");
-    footer.textContent = `\u00A9 ${new Date().getFullYear()} Clothing Store`;
-  
+    footer.textContent = `\u00A9 ${new Date().getFullYear()} Clothing Store`; // Update the footer text with the current year
+
     // Make the image bigger on mouseover and revert on mouseout
     const productImages = document.querySelectorAll(".product img");
     productImages.forEach(function (image) {
-      image.addEventListener("mouseover", function () {
-        // Increase the size of the image on mouseover
-        image.style.width = "50%"; // You can adjust the size based on your needs
-      });
-  
-      image.addEventListener("mouseout", function () {
-        // Revert the size of the image on mouseout
-        image.style.width = "25%"; // Set it back to the original size or adjust as needed
-      });
-    });
-  });
-  
+        image.addEventListener("mouseover", function () {
+            // Increase the size of the image on mouseover
